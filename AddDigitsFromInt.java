65dabf67d5a19c3a5ee0a5b18e7080ffb2dd9988
Valeriia Morkhat <mssolipsismus@gmail.com>
2023-01-19 19:21:09 +0300

    new test

---
+++ AddDigitsFromInt.java
@@ -0,0 +1,15 @@
+public class AddDigitsFromInt {
+	public static int addDigitsFromInt(int number) {
+		var result = 0;
+		while (number > 0) { // use while loop since the number of iterations is unknown & we don't need an iterable, only expression that should be true 
+			result = result + number % 10; // remainder operator is used to sum the smallest digits
+			number = number / 10; // move to the next radix 
+		}
+		result = (result < 10) ? result : addDigitsFromInt(result); // start another iteration if result has more than one radix
+		return result;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(addDigitsFromInt(99)); // should be 9
+	}
+}
