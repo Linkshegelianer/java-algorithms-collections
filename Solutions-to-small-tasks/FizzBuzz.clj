a7e20261f407c3ea26a9024608caa42645f686de
Lera <108754223+Linkshegelianer@users.noreply.github.com>
2023-01-21 20:00:25 +0300

    Delete FizzBuzz.clj

--- Solutions-to-small-tasks/FizzBuzz.clj
+++
@@ -1,6 +0,0 @@
-(doseq [i (range 1 50)]
-  (cond 
-        (zero? (mod i 15)) (println "FizzBuzz")
-        (zero? (mod i 3)) (println "Fizz")
-        (zero? (mod i 5)) (println "Buzz")
-        :else (println i)))
